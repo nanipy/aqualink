5f1c87bffbdc4da3197e492e325befea5cdc206d
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-12-09 18:42:58 +0200

    ->

--- waterlink/player.py
+++ waterlink/player.py
@@ -1,2 +1,7 @@
 class Player:
-    pass
+    __slots__ = (
+        'conn', '_guild', '_channel', '_paused', '_playing', '_position', '_volume', '_track_callback', '_connecting'
+    )
+    
+    def __init__(self):
+        pass
